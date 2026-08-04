994e2a022ef6954b945da4fdb9009bd1f9abccbb
alex-marco <marcoalex201804@gmail.com>
2026-08-04 12:37:55 +0100

    fix(infra): discover existing endpoint rules by ID

--- infrastructure/terraform/environments/staging/imports.tf
+++ infrastructure/terraform/environments/staging/imports.tf
@@ -67,38 +67,32 @@ data "aws_security_group" "existing_vpc_endpoints" {
   vpc_id = module.vpc.vpc_id
 }
 
-data "aws_vpc_security_group_rule" "existing_vpc_endpoints_https" {
-  for_each = toset(["10.0.10.0/24", "10.0.11.0/24"])
+locals {
+  existing_vpc_endpoint_https_cidrs = toset(["10.0.10.0/24", "10.0.11.0/24"])
+  existing_vpc_endpoint_https_rule_ids = {
+    for rule in data.aws_vpc_security_group_rule.existing_vpc_endpoints :
+    coalesce(rule.cidr_ipv4, "") => rule.security_group_rule_id
+    if(
+      !rule.is_egress &&
+      rule.ip_protocol == "tcp" &&
+      rule.from_port == 443 &&
+      rule.to_port == 443 &&
+      contains(local.existing_vpc_endpoint_https_cidrs, rule.cidr_ipv4)
+    )
+  }
+}
 
+data "aws_vpc_security_group_rules" "existing_vpc_endpoints" {
   filter {
     name   = "group-id"
     values = [data.aws_security_group.existing_vpc_endpoints.id]
   }
+}
 
-  filter {
-    name   = "is-egress"
-    values = ["false"]
-  }
+data "aws_vpc_security_group_rule" "existing_vpc_endpoints" {
+  for_each = toset(data.aws_vpc_security_group_rules.existing_vpc_endpoints.ids)
 
-  filter {
-    name   = "ip-protocol"
-    values = ["tcp"]
-  }
-
-  filter {
-    name   = "from-port"
-    values = ["443"]
-  }
-
-  filter {
-    name   = "to-port"
-    values = ["443"]
-  }
-
-  filter {
-    name   = "cidr"
-    values = [each.value]
-  }
+  security_group_rule_id = each.value
 }
 
 data "aws_vpc_endpoint" "existing_s3" {
@@ -121,10 +115,10 @@ import {
 }
 
 import {
-  for_each = data.aws_vpc_security_group_rule.existing_vpc_endpoints_https
+  for_each = local.existing_vpc_endpoint_https_rule_ids
 
   to = module.vpc.aws_vpc_security_group_ingress_rule.vpc_endpoints_https[each.key]
-  id = each.value.security_group_rule_id
+  id = each.value
 }
 
 import {
